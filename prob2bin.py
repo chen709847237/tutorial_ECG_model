f29b588b07694ac1d861359a8e5ab04ded40b8a6
Ray_78516 <31036033+chen709847237@users.noreply.github.com>
2021-02-11 00:05:17 +0800

    Update prob2bin.py

--- prob2bin.py
+++ prob2bin.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 if __name__ == '__main__':
 
-    data_path = '/Users/chenjiarui/Python Dev/Lab_project/ECG/automatic-ecg-diagnosis-master/dnn_output.npy'
-    outout_path = '/Users/chenjiarui/Python Dev/Lab_project/ECG/automatic-ecg-diagnosis-master/dnn_output_bin.csv'
+    data_path = '<Replace this string with the path of prediction result file (dnn_output.npy)>'
+    outout_path = '<Replace this string with the path of output transformed file path/output transformed file name>'
     decision_threshold = [0.12361142, 0.09042355, 0.0513474, 0.26380765, 0.36633369, 0.17381361]
     prediction_result = np.load(data_path, allow_pickle=True)
     for i in range(prediction_result.shape[1]):
