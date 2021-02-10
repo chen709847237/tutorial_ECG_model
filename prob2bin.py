6adc21abd2d530e3f3245db3fb9ce73e9a4841c4
Ray_78516 <31036033+chen709847237@users.noreply.github.com>
2021-02-11 00:08:11 +0800

    Update prob2bin.py

--- prob2bin.py
+++ prob2bin.py
@@ -1,11 +1,11 @@
+# Transform the prediction results from probability to binary label.
 import numpy as np
 import pandas as pd
-
 
 if __name__ == '__main__':
 
     data_path = '<Replace this string with the path of prediction result file (dnn_output.npy)>'
-    outout_path = '<Replace this string with the path of output transformed file path/output transformed file name>'
+    outout_path = '<Replace this string with the path of output transformed file path + / + output transformed file name>'
     decision_threshold = [0.12361142, 0.09042355, 0.0513474, 0.26380765, 0.36633369, 0.17381361]
     prediction_result = np.load(data_path, allow_pickle=True)
     for i in range(prediction_result.shape[1]):
