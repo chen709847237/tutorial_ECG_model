2f6b0a396995235bda24a29be0f741c832c589ea
Ray_78516 <31036033+chen709847237@users.noreply.github.com>
2021-02-26 21:40:17 +0800

    Update script_7_prob2bin.py

--- script_7_prob2bin.py
+++ script_7_prob2bin.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
-    data_path = '<Replace this string with the path of prediction result file (dnn_output.npy)'
+    data_path = '<Replace this string with the path of prediction result file (dnn_output.npy)>'
     outout_path = '<Replace this string with the path of output transformed file path + / + output transformed file name>'
     decision_threshold = [0.12361142, 0.09042355, 0.0513474, 0.26380765, 0.36633369, 0.17381361]
     prediction_result = np.load(data_path, allow_pickle=True)
