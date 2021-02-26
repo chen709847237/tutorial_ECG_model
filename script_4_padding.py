f8b2e8ccd0855cd935742581f55dd6973da79c09
Ray_78516 <31036033+chen709847237@users.noreply.github.com>
2021-02-27 03:00:37 +0800

    Update script_4_padding.py

--- script_4_padding.py
+++ script_4_padding.py
@@ -9,9 +9,8 @@ import os
 import numpy as np
 import pandas as pd
 
-
-data_root_folder = '/Users/chenjiarui/Python Dev/Lab_project/ECG/ECG_data_csv_400Hz_scale_60/'
-output_root_folder = '/Users/chenjiarui/Python Dev/Lab_project/ECG/ECG_data_csv_400Hz_scale_60_pad/'
+data_root_folder = '<Replace this string with the path of the folder where the ECG signal csv files are stored>'
+output_root_folder = '<Replace this string with the path of the output target folder>'
 
 data_file_list = os.listdir(data_root_folder)
 if '.DS_Store' in data_file_list: data_file_list.remove('.DS_Store')
@@ -19,27 +18,15 @@ count = 0
 for data_file in data_file_list:
 
     data = pd.read_csv(data_root_folder+data_file, index_col='samplenr')
-
     col_name = data.columns.tolist()
     temp_dict = {}
     for lead_name in col_name:
-
         pad_data = np.pad(data[lead_name].values, ((48, 48)), mode='constant')
         temp_dict[lead_name] = pad_data
 
-        #break
-
     target_data = pd.DataFrame(temp_dict)
     target_data.index.name = 'samplenr'
-
     target_data.to_csv(output_root_folder+data_file)
-
     count += 1
     print('file:', count)
-
-    #break
-
-
-
-
 
