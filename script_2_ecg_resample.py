c37322b94aa1eac43be31cf899ab18eca99362e7
Ray_78516 <31036033+chen709847237@users.noreply.github.com>
2021-02-27 02:53:51 +0800

    Update script_2_ecg_resample.py

--- script_2_ecg_resample.py
+++ script_2_ecg_resample.py
@@ -4,13 +4,14 @@
 # Time: 2021/2/26
 # Part of ECG model tutorial @ Github: https://github.com/chen709847237/tutorial_ECG_model
 # Script for ECG signals resampling
+
 import os
 from wfdb import processing
 import numpy as np
 import pandas as pd
 
-data_root_folder = '/Users/chenjiarui/Python Dev/Lab_project/ECG/ECG_data_csv/'
-output_root_folder = '/Users/chenjiarui/Python Dev/Lab_project/ECG/ECG_data_csv_400Hz/'
+data_root_folder = '<Replace this string with the path of the folder where the 500 Hz csv files are stored>'
+output_root_folder = '<Replace this string with the path of the output target folder>'
 
 data_file_list = os.listdir(data_root_folder)
 if '.DS_Store' in data_file_list: data_file_list.remove('.DS_Store')
@@ -28,9 +29,5 @@ for data_file in data_file_list:
     target_data.to_csv(output_root_folder+data_file)
     count += 1
     print('file:', count)
-
-
-
-
 
 
