3ebd684fb3940399fa6e1bdfc70a486ac79a87d6
Ray_78516 <31036033+chen709847237@users.noreply.github.com>
2021-02-27 02:58:43 +0800

    Update script_3_scale.py

--- script_3_scale.py
+++ script_3_scale.py
@@ -9,34 +9,23 @@ import os
 import numpy as np
 import pandas as pd
 
-data_root_folder = '/Users/chenjiarui/Python Dev/Lab_project/ECG/ECG_data_csv_400Hz/'
-output_root_folder = '/Users/chenjiarui/Python Dev/Lab_project/ECG/ECG_data_csv_400Hz_scale_60/'
+data_root_folder = '<Replace this string with the path of the folder where the 400 Hz csv files are stored>'
+output_root_folder = '<Replace this string with the path of the output target folder>'
 
 data_file_list = os.listdir(data_root_folder)
 if '.DS_Store' in data_file_list: data_file_list.remove('.DS_Store')
 count = 0
 for data_file in data_file_list:
-
     data = pd.read_csv(data_root_folder+data_file, index_col='samplenr')
-
     col_name = data.columns.tolist()
     temp_dict = {}
     for lead_name in col_name:
-
         scale_data = np.array(data[lead_name].tolist()) / 600
         temp_dict[lead_name] = scale_data
 
     target_data = pd.DataFrame(temp_dict)
     target_data.index.name = 'samplenr'
-
     target_data.to_csv(output_root_folder+data_file)
-
     count += 1
     print('file:', count)
-
-    #break
-
-
-
-
 
